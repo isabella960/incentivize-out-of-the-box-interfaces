c53c719002c9f2803b0ae99d27ec8a20f0c27957
Isabella <143600689+isabella960@users.noreply.github.com>
2023-11-23 10:25:20 +0000

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,40 @@
-        int[] data = new int[10];
 
-        }
-public class RandomDataGenerator {
-}
-    public static void main(String[] args) {
-            System.out.println("Random Number: " + item);
 
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int item : data) {
-    }
-        for (int i = 0; i < 10; i++) {
-        }
+def generate_random_data():
+    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+def main():
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+import random
+    data = generate_random_data()if __name__ == "__main__":
+    for item in data:
+import random
+def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+    return data
+def generate_random_data():
+
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+def main():
+    main()
+
+import random
+    return data
+        print(f"Random Number: {item}")
+    data = generate_random_data()
