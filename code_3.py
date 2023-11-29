5ab262b680a24b2b17cc0b245812986733e001dc
Isabella <143600689+isabella960@users.noreply.github.com>
2023-11-29 00:55:24 +0000

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,40 +1,14 @@
+def main():
 
-
+import random
 def generate_random_data():
-    main()
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-
-def main():
-
         print(f"Random Number: {item}")
-if __name__ == "__main__":
-import random
-    data = generate_random_data()if __name__ == "__main__":
-    for item in data:
-import random
-def main():
     main()
-    data = [random.randint(1, 100) for _ in range(10)]
-
     data = generate_random_data()
-        print(f"Random Number: {item}")
-
-def generate_random_data():
-
-    return data
-def generate_random_data():
-
-    for item in data:
 
     data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
 
 if __name__ == "__main__":
-def main():
-    main()
-
-import random
     return data
-        print(f"Random Number: {item}")
-    data = generate_random_data()
